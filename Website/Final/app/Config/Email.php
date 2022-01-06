471bd9745ab188b21051602546e24d9a9f0c9c9c
chris5zk <wzk789wzk@gmail.com>
2022-01-06 14:58:14 +0800

    register validation complete

--- Website/Final/app/Config/Email.php
+++ Website/Final/app/Config/Email.php
@@ -33,7 +33,7 @@ class Email extends BaseConfig
      *
      * @var string
      */
-    public $protocol = 'mail';
+    public $protocol = 'smtp';
 
     /**
      * The server path to Sendmail.
@@ -47,35 +47,35 @@ class Email extends BaseConfig
      *
      * @var string
      */
-    public $SMTPHost;
+    public $SMTPHost = 'smtp.gnail.com';
 
     /**
      * SMTP Username
      *
      * @var string
      */
-    public $SMTPUser;
+    public $SMTPUser = 'wzk789wzk@alum.ccu.edu.tw';
 
     /**
      * SMTP Password
      *
      * @var string
      */
-    public $SMTPPass;
+    public $SMTPPass = '89115wzk';
 
     /**
      * SMTP Port
      *
      * @var int
      */
-    public $SMTPPort = 25;
+    public $SMTPPort = 465;
 
     /**
      * SMTP Timeout (in seconds)
      *
      * @var int
      */
-    public $SMTPTimeout = 5;
+    public $SMTPTimeout = 20;
 
     /**
      * Enable persistent SMTP connections
@@ -89,7 +89,7 @@ class Email extends BaseConfig
      *
      * @var string
      */
-    public $SMTPCrypto = 'tls';
+    public $SMTPCrypto;
 
     /**
      * Enable word-wrap
@@ -110,7 +110,7 @@ class Email extends BaseConfig
      *
      * @var string
      */
-    public $mailType = 'text';
+    public $mailType = 'html';
 
     /**
      * Character set (utf-8, iso-8859-1, etc.)
--- Website/Final/app/Views/login_system/register.php
+++ Website/Final/app/Views/login_system/register.php
@@ -2,21 +2,32 @@
 <?= $this->section('content') ?>
 
 <head>
-
+    <link rel="stylesheet" href="/style/login_system.css">
 </head>
 
 <body>
-    <div>
-        <form action="/LoginController/new_account" enctype="multipart/form-data" method="post">
-            <h5>ACCOUNT:</h5>
-            <input id="account" type="text" name="account">
-            <br>
-            <h5>PASSWORD:</h5>
-            <input id="password" type="text" name="password">
-            <button>Submit</button>
+    <?php if (isset($validation)) : ?>
+        <div class="validation">
+            <?= $validation->listErrors()?>
+        </div>
+    <?php endif; ?>
+
+    <div class="center">
+        <form action="/LoginController/new_account" enctype="multipart/form-data" method="POST">
+            <h2>暱稱:</h2>
+            <input id="name" type="text" name="name" placeholder="請輸入暱稱">
+            <h2>EMAIL:</h2>
+            <input id="mail" type="email" name="mail" placeholder="請輸入電子郵件">
+            <h2>帳號:</h2>
+            <input id="account" type="text" name="account" placeholder="請輸入帳號">
+            <h2>密碼:</h2>
+            <input id="password" type="password" name="password" placeholder="請輸入密碼">
+            <h2>再次輸入密碼:</h2>
+            <input id="passconf" type="password" name="passconf" placeholder="請再次輸入密碼">
+            <br><br>
+            <button>註冊</button>
         </form>
     </div>
 </body>
-
 
 <?= $this->endSection() ?>
