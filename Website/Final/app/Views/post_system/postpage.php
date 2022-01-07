f1cd003901b0869c807090301733652baa3b7a36
boyuchen3323 <threeboys3323@gmail.com>
2022-01-07 11:37:26 +0800

    mistake..

--- Website/Final/app/Views/post_system/postpage.php
+++ Website/Final/app/Views/post_system/postpage.php
@@ -6,7 +6,12 @@
 </head>
 
 <body>
-    <a href="#">HI</a>
+    <form>
+    請輸入文字:
+    <input type="text" id="text" name="text_name" style="width: 100%; height: 400px; " /> 
+    </form>
+
+	<a href="#top">回到頂端</a>
 </body>
 
 <?= $this->endSection() ?>
